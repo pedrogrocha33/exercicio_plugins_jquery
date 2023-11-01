b684a6198cbede136c1811f8c7060ed049db99a5
Pedro Rocha <pedro2022bd@icloud.com>
2023-11-01 01:50:58 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    });
+
+    
+    
+    $('#telefone').mask('(00) 00000-0000', {
+        
+        placeholder: '(14) 99999-9999'
+    })
+    $('#CPF').mask('000.000.000-00', {
+        
+        placeholder: '000.000.000-00'
+    })
+    $('#CEP').mask('00.000-000', {
+        
+        placeholder: '00.000-000'
+    })
+    $('#telefone').mask('(00) 00000-0000', {
+        
+        placeholder: '(14) 99999-9999'
+    })
+    
+
+
+
